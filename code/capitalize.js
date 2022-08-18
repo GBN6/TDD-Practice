f00caee11cff6565c33f6aec09afd6c4a8785ac9
GBN6 <czesiox8@gmail.com>
2022-08-18 13:40:12 +0200

    Create capitalize function and test it
    Capitalze function takes string and returns the first
    character capitalized, leaving rest of the string with
    lower case

--- code/capitalize.js
+++ code/capitalize.js
@@ -1,5 +1,5 @@
 function capitalize(strn) {
-
+    return strn.charAt(0).toUpperCase() + strn.slice(1).toLowerCase();
 }
 
 export default capitalize
--- tests/capitalzie.test.js
+++ tests/capitalzie.test.js
@@ -1,5 +1,17 @@
 import capitalize from "../code/capitalize";
 
 test('Capitalize first letter', () => {
-    expect(capitalize('hello').toBe('Hello'))
+    expect(capitalize('hello')).toBe('Hello')
+})
+
+test('First letter already capitalized', () => {
+    expect(capitalize('Telephone')).toBe('Telephone')
+})
+
+test('First letter small, another letters big', () => {
+    expect(capitalize('hi My Name Is Briana')).toBe('Hi my name is briana')
+})
+
+test(" '' to be '' ", () =>{
+    expect(capitalize('')).toBe('');
 })
